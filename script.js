83c006aefcbd9d0a16f3bdc9e97f10542676bcad
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2022-09-26 11:06:08 +0200

    Added playRound function

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ function getComputerChoice() {
 	const randomIndex = Math.floor(Math.random() * 3);
 	return SHAPES[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+	playerSelection = playerSelection.toLowerCase();
+
+	if (playerSelection == SHAPES[0]) {
+		if (computerSelection == SHAPES[0]) {
+			return `Tie!`;
+		} else if (computerSelection == SHAPES[1]) {
+			return `You Lose! ${computerSelection} beats ${playerSelection}`;
+		} else {
+			return `You Win! ${playerSelection} beats ${computerSelection}`;
+		}
+	} else if (playerSelection == SHAPES[1]) {
+		if (computerSelection == SHAPES[0]) {
+			return `You Win! ${playerSelection} beats ${computerSelection}`;
+		} else if (computerSelection == SHAPES[1]) {
+			return `Tie!`;
+		} else {
+			return `You Lose! ${computerSelection} beats ${playerSelection}`;
+		}
+	} else {
+		if (computerSelection == SHAPES[0]) {
+			return `You Lose! ${computerSelection} beats ${playerSelection}`;
+		} else if (computerSelection == SHAPES[1]) {
+			return `You Win! ${playerSelection} beats ${computerSelection}`;
+		} else {
+			return `Tie!`;
+		}
+	}
+}
